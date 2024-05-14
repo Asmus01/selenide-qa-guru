2f225fef5460fd59b12bf1db5f6945c2e181999a
AslanMus <asmus01@mail.ru>
2024-05-14 14:11:36 +0300

    withAllure

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,10 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import net.bytebuddy.pool.TypePool;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.HashMap;
@@ -20,5 +24,16 @@ public class TestBase {
             put("enableVideo", true);
         }});
         Configuration.browserCapabilities = capabilities;
+    }
+    @BeforeEach
+    void addListenerAllure() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
